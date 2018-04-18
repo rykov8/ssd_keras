a82b3ae27f27d98b8d5233fba88c589ced222c45
Markus Völk <markus.voelk@gmx.de>
2018-04-18 09:17:33 +0200

    add SegLink videotest

---
+++ sl_videotest.py
@@ -0,0 +1,150 @@
+""" A class for testing a SSD model on a video file or webcam """
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import keras
+import pickle
+from timeit import default_timer as timer
+
+from ssd_data import preprocess
+from sl_utils import rbox_to_polygon
+
+
+class VideoTest(object):
+    """ Class for testing a trained SSD model on a video file and show the
+        result in a window. Class is designed so that one VideoTest object 
+        can be created for a model, and the same object can then be used on 
+        multiple videos and webcams.
+        
+        Arguments:
+            class_names: A list of strings, each containing the name of a class.
+                         The first name should be that of the background class
+                         which is not used.
+                         
+            model:       An SSD model. It should already be trained for 
+                         images similar to the video to test on.
+                         
+            input_shape: The shape that the model expects for its input, 
+                         as a tuple, for example (300, 300, 3)    
+                         
+            bbox_util:   An instance of the BBoxUtility class in ssd_utils.py
+                         The BBoxUtility needs to be instantiated with 
+                         the same number of classes as the length of        
+                         class_names.
+    
+    """
+    
+    def __init__(self, model, prior_util, class_names, input_shape):
+        self.class_names = class_names
+        self.num_classes = len(class_names)
+        self.model = model
+        self.input_shape = input_shape
+        self.prior_util = prior_util
+        
+        # use same colors as in plots
+        colors = plt.cm.hsv(np.linspace(0, 1, self.num_classes+1))
+        colors = (colors[:,(2,1,0)]*255).astype(np.int32).tolist()
+        self.class_colors = colors
+        
+    def run(self, video_path=0, start_frame=0, segment_threshold=0.9, link_threshold=0.7):
+        """ Runs the test on a video (or webcam)
+        
+        # Arguments
+        video_path: A file path to a video to be tested on. Can also be a number, 
+                    in which case the webcam with the same number (i.e. 0) is 
+                    used instead
+                    
+        start_frame: The number of the first frame of the video to be processed
+                     by the network. 
+                     
+        conf_thresh: Threshold of confidence. Any boxes with lower confidence 
+                     are not visualized.
+                    
+        """
+    
+        vid = cv2.VideoCapture(video_path)
+        if not vid.isOpened():
+            raise IOError(("Couldn't open video file or webcam. If you're "
+            "trying to open a webcam, make sure you video_path is an integer!"))
+        
+        vid_w = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        vid_h = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        
+        # Skip frames until reaching start_frame
+        if start_frame > 0:
+            vid.set(cv2.CAP_PROP_POS_MSEC, start_frame)
+            
+        accum_time = 0
+        curr_fps = 0
+        fps = "FPS: ??"
+        prev_time = timer()
+        
+        input_size = self.input_shape[:2]
+        
+        while True:
+            retval, img = vid.read()
+            if not retval:
+                print("Done!")
+                return
+                
+            # model to predict 
+            x = np.array([preprocess(img, input_size)])
+            y = self.model.predict(x)
+            
+            result = self.prior_util.decode(y[0], segment_threshold=0.7, link_threshold=0.01)
+            
+            for r in result:
+                xy = rbox_to_polygon(r)
+                xy = xy / input_size * [vid_w, vid_h]
+                xy = xy.reshape((-1,1,2))
+                xy = np.round(xy)
+                xy = xy.astype(np.int32)
+                cv2.polylines(img, [xy], True, (0,0,255))
+                
+            # Calculate FPS
+            # This computes FPS for everything, not just the model's execution 
+            # which may or may not be what you want
+            curr_time = timer()
+            exec_time = curr_time - prev_time
+            prev_time = curr_time
+            accum_time = accum_time + exec_time
+            curr_fps = curr_fps + 1
+            if accum_time > 1:
+                accum_time = accum_time - 1
+                fps = "FPS: " + str(curr_fps)
+                curr_fps = 0
+            
+            # Draw FPS in top left corner
+            cv2.rectangle(img, (0,0), (50, 17), (255,255,255), -1)
+            cv2.putText(img, fps, (3,10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,0,0), 1)
+            
+            cv2.imshow("SegLink detection", img)
+            cv2.waitKey(10)
+
+
+if __name__ == '__main__':
+
+    from sl_model import SL512 as Model
+    from sl_utils import PriorUtil
+
+    class_names = ['Background', 'Text'];
+    input_shape = (512,512,3)
+    model = Model(input_shape, num_classes=len(class_names))
+    prior_util = PriorUtil(model)
+
+    #experiment = '201709091812_sl_test'
+    #epoch = 11
+    experiment = '201711111814_sl512_synthtext_focal'
+    epoch = 1
+    model.load_weights('checkpoints/%s/weights.%03d.h5' % (experiment, epoch), by_name=True)
+    
+    vid_test = VideoTest(model, prior_util, class_names, input_shape)
+
+    # To test on webcam 0, /dev/video0
+    try:
+        #vid_test.run('data/video.mp4', start_frame=100)
+        vid_test.run(0)
+    except KeyboardInterrupt:
+        pass
+    
