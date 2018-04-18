c1663cac2b5a2b4e49470c0299c69d5ebb83a898
Markus Völk <markus.voelk@gmx.de>
2018-04-18 07:07:26 +0200

    add focal loss for SegLink

--- sl_training.py
+++ sl_training.py
@@ -155,3 +155,96 @@ class SegLinkLoss(object):
         
         return total_loss
 
+
+class SegLinkFocalLoss(object):
+
+    def __init__(self, lambda_segments=100.0, lambda_offsets=1.0, lambda_links=100.0, gamma_segments=2, gamma_links=2):
+        self.lambda_segments = lambda_segments
+        self.lambda_offsets = lambda_offsets
+        self.lambda_links = lambda_links
+        self.gamma_segments = gamma_segments
+        self.gamma_links = gamma_links
+        self.metrics = []
+    
+    def compute(self, y_true, y_pred):
+        # y.shape (batches, segments, 2 x segment_label + 5 x segment_offset + 16 x inter_layer_links_label + 8 x cross_layer_links_label)
+        
+        offset = 64*64 # depends on first map_size
+        batch_size = tf.shape(y_true)[0]
+        eps = K.epsilon()
+        
+        # segment confidence loss
+        seg_conf_true = tf.reshape(y_true[:,:,0:2], [-1, 2])
+        seg_conf_pred = tf.reshape(y_pred[:,:,0:2], [-1, 2])
+        num_seg = tf.shape(seg_conf_true)[0]
+        
+        pos_seg_mask = seg_conf_true[:,1]
+        pos_seg_mask_float = tf.cast(pos_seg_mask, tf.float32)
+        num_pos_seg = tf.reduce_sum(pos_seg_mask_float)
+        
+        seg_conf_loss = focal_loss(seg_conf_true, seg_conf_pred, self.gamma_segments)
+        seg_conf_loss = tf.reduce_sum(seg_conf_loss)
+        seg_conf_loss = seg_conf_loss / (tf.cast(num_seg, tf.float32) + eps)
+        seg_conf_loss = self.lambda_segments * seg_conf_loss
+        
+        # segment offset loss
+        seg_loc_true = tf.reshape(y_true[:,:,2:7], [-1, 5])
+        seg_loc_pred = tf.reshape(y_pred[:,:,2:7], [-1, 5])
+        
+        seg_loc_loss = smooth_l1_loss(seg_loc_true, seg_loc_pred)
+        pos_seg_loc_loss = tf.reduce_sum(seg_loc_loss * pos_seg_mask_float)
+        
+        seg_loc_loss = pos_seg_loc_loss / (num_pos_seg + eps)
+        seg_loc_loss = self.lambda_offsets * seg_loc_loss
+        
+        # link confidence loss
+        inter_link_conf_true = y_true[:,:,7:23]
+        cross_link_conf_true = y_true[:,offset:,23:31]
+        link_conf_true = tf.concat([tf.reshape(inter_link_conf_true, [-1, 2]), 
+                                    tf.reshape(cross_link_conf_true, [-1, 2])], 0)
+        inter_link_conf_pred = y_pred[:,:,7:23]
+        cross_link_conf_pred = y_pred[:,offset:,23:31]
+        link_conf_pred = tf.concat([tf.reshape(inter_link_conf_pred, [-1, 2]), 
+                                    tf.reshape(cross_link_conf_pred, [-1, 2])], 0)
+        num_link = tf.shape(link_conf_true)[0]
+        
+        link_conf_loss = focal_loss(link_conf_true, link_conf_pred, self.gamma_links)
+        link_conf_loss = tf.reduce_sum(link_conf_loss)
+        link_conf_loss = link_conf_loss / (tf.cast(num_link, tf.float32) + eps)
+        link_conf_loss = self.lambda_links * link_conf_loss
+        
+        # total loss
+        total_loss = seg_conf_loss + seg_loc_loss + link_conf_loss
+        
+        
+        seg_conf = tf.reduce_max(seg_conf_pred, axis=1)
+        seg_class_true = tf.argmax(seg_conf_true, axis=1)
+        seg_class_pred = tf.argmax(seg_conf_pred, axis=1)
+        seg_precision, seg_recall, seg_accuracy, seg_fmeasure = compute_metrics(seg_class_true, seg_class_pred, seg_conf)
+        
+        link_conf = tf.reduce_max(link_conf_pred, axis=1)
+        link_class_true = tf.argmax(link_conf_true, axis=1)
+        link_class_pred = tf.argmax(link_conf_pred, axis=1)
+        link_precision, link_recall, link_accuracy, link_fmeasure = compute_metrics(link_class_true, link_class_pred, link_conf)
+        
+        # metrics
+        def make_fcn(t):
+            return lambda y_true, y_pred: t
+        for name in ['seg_conf_loss', 
+                     'seg_loc_loss', 
+                     'link_conf_loss', 
+                     
+                     'seg_precision', 
+                     'seg_recall', 
+                     'seg_accuracy', 
+                     'seg_fmeasure',
+                     'link_precision', 
+                     'link_recall', 
+                     'link_accuracy', 
+                     'link_fmeasure',
+                    ]:
+            f = make_fcn(eval(name))
+            f.__name__ = name
+            self.metrics.append(f)
+        
+        return total_loss
