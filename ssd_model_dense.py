c783918542e97ae386f5ff0c25c2a4701981437c
Markus Völk <markus.voelk@gmx.de>
2018-09-04 14:37:24 +0200

    refactoring dense blocks

--- ssd_model_dense.py
+++ ssd_model_dense.py
@@ -15,14 +15,18 @@ def bn_acti_conv(x, filters, kernel_size=1, stride=1, padding='same', activation
     x = BatchNormalization(scale=True)(x)
     x = Activation(activation)(x)
     x = Conv2D(filters, kernel_size, strides=stride, padding=padding)(x)
-    #x = Dropout(0.5)(x)
     return x
 
-def bl_layer1(x, filters, width, activation='relu'):
-    x1 = x
-    x2 = bn_acti_conv(x, filters*width, 1, 1, activation=activation)
-    x2 = bn_acti_conv(x2, filters, 3, 1, activation=activation)
-    return concatenate([x1, x2], axis=3)
+def dense_block(x, n, growth_rate, width=4, activation='relu'):
+    input_shape = K.int_shape(x)
+    c = input_shape[3]
+    for i in range(n):
+        x1 = x
+        x2 = bn_acti_conv(x, growth_rate*width, 1, 1, activation=activation)
+        x2 = bn_acti_conv(x2, growth_rate, 3, 1, activation=activation)
+        x = concatenate([x1, x2], axis=3)
+        c += growth_rate
+    return x
 
 def bl_layer2(x, filters, width, padding='same', activation='relu'):
     x1 = MaxPooling2D(pool_size=2, strides=2, padding=padding)(x)
@@ -37,6 +41,10 @@ def dsod300_body(x, activation='relu'):
     if activation == 'leaky_relu':
         activation = leaky_relu
     
+    growth_rate = 48
+    compression = 1.0
+    source_layers = []
+    
     # Stem
     x = Conv2D(64, 3, strides=2, padding='same')(x)
     x = BatchNormalization(scale=True)(x)
@@ -48,42 +56,26 @@ def dsod300_body(x, activation='relu'):
     x = BatchNormalization(scale=True)(x)
     x = Activation(activation)(x)
     x = MaxPooling2D(pool_size=2, strides=2)(x)
-    
-    growth_rate = 48
-    num_channels = 128
-    source_layers = []
-
-    for i in range(6):
-        x = bl_layer1(x, growth_rate, 4, activation=activation)
-        num_channels += growth_rate
-
-    x = bn_acti_conv(x, num_channels, 1, 1, activation=activation)
+    # Dense Block 1
+    x = dense_block(x, 6, growth_rate, 4, activation)
+    x = bn_acti_conv(x, int(K.int_shape(x)[3]*compression), 1, 1, activation=activation)
     x = MaxPooling2D(pool_size=2, strides=2, padding='same')(x)
-
-    for i in range(8):
-        x = bl_layer1(x, growth_rate, 4, activation=activation)
-        num_channels += growth_rate
-
-    x = bn_acti_conv(x, num_channels, 1, 1, activation=activation)
-    source_layers.append(x) # 38x38x512
-
-    x = MaxPooling2D(pool_size=2, strides=2)(x)
-
-    for i in range(8):
-        x = bl_layer1(x, growth_rate, 4, activation=activation)
-        num_channels += growth_rate
-
-    x = bn_acti_conv(x, num_channels, 1, 1, activation=activation)
-
-    for i in range(8):
-        x = bl_layer1(x, growth_rate, 4, activation=activation)
-        num_channels += growth_rate
-
+    # Dense Block 2
+    x = dense_block(x, 8, growth_rate, 4, activation)
+    x = bn_acti_conv(x, int(K.int_shape(x)[3]*compression), 1, 1, activation=activation)
+    source_layers.append(x) # 38x38x...
+    
+    x = x2 = MaxPooling2D(pool_size=2, strides=2)(x)
+    # Dense Block 3
+    x = dense_block(x, 8, growth_rate, 4, activation)
+    x = bn_acti_conv(x, int(K.int_shape(x)[3]*compression), 1, 1, activation=activation)
+    # Dense Block 4
+    x = dense_block(x, 8, growth_rate, 4, activation)
+    
     x1 = bn_acti_conv(x, 256, 1, 1, activation=activation)
-    x2 = MaxPooling2D(pool_size=2, strides=2)(source_layers[0])
     x2 = bn_acti_conv(x2, 256, 1, 1, activation=activation)
     x = concatenate([x1, x2], axis=3)
-    source_layers.append(x) # 19x19x1024
+    source_layers.append(x) # 19x19x512
 
     x = bl_layer2(x, 256, 1, activation=activation)
     source_layers.append(x) # 10x10x512
@@ -94,7 +86,7 @@ def dsod300_body(x, activation='relu'):
     x = bl_layer2(x, 128, 1, activation=activation)
     source_layers.append(x) # 3x3x256
 
-    x = bl_layer2(x, 128, 1, 'valid', activation=activation)
+    x = bl_layer2(x, 128, 1, padding='valid', activation=activation)
     source_layers.append(x) # 1x1x256
     
     return source_layers
@@ -104,6 +96,10 @@ def dsod512_body(x, activation='relu'):
     
     if activation == 'leaky_relu':
         activation = leaky_relu
+    
+    growth_rate = 48
+    compression = 1.0
+    source_layers = []
     
     # Stem
     x = Conv2D(64, 3, strides=2, padding='same')(x)
@@ -117,41 +113,26 @@ def dsod512_body(x, activation='relu'):
     x = Activation(activation)(x)
     x = MaxPooling2D(pool_size=2, strides=2)(x)
     
-    growth_rate = 48
-    num_channels = 128
-    source_layers = []
-
-    for i in range(6):
-        x = bl_layer1(x, growth_rate, 4)
-        num_channels += growth_rate
-
-    x = bn_acti_conv(x, num_channels, 1, 1, activation=activation)
+    # Dense Block 1
+    x = dense_block(x, 6, growth_rate, 4, activation)
+    x = bn_acti_conv(x, int(K.int_shape(x)[3]*compression), 1, 1, activation=activation)
     x = MaxPooling2D(pool_size=2, strides=2, padding='same')(x)
+    # Dense Block 2
+    x = dense_block(x, 8, growth_rate, 4, activation)
+    x = bn_acti_conv(x, int(K.int_shape(x)[3]*compression), 1, 1, activation=activation)
+    source_layers.append(x) # 64x64x...
 
-    for i in range(8):
-        x = bl_layer1(x, growth_rate, 4)
-        num_channels += growth_rate
-
-    x = bn_acti_conv(x, num_channels, 1, 1, activation=activation)
-    source_layers.append(x) # 64x64x512
-
-    x = MaxPooling2D(pool_size=2, strides=2)(x)
-
-    for i in range(8):
-        x = bl_layer1(x, growth_rate, 4)
-        num_channels += growth_rate
-
-    x = bn_acti_conv(x, num_channels, 1, 1, activation=activation)
-
-    for i in range(8):
-        x = bl_layer1(x, growth_rate, 4)
-        num_channels += growth_rate
-
+    x = x2 = MaxPooling2D(pool_size=2, strides=2)(x)
+    # Dense Block 3
+    x = dense_block(x, 8, growth_rate, 4, activation)
+    x = bn_acti_conv(x, int(K.int_shape(x)[3]*compression), 1, 1, activation=activation)
+    # Dense Block 4
+    x = dense_block(x, 8, growth_rate, 4, activation)
+    
     x1 = bn_acti_conv(x, 256, 1, 1, activation=activation)
-    x2 = MaxPooling2D(pool_size=2, strides=2)(source_layers[0])
     x2 = bn_acti_conv(x2, 256, 1, 1, activation=activation)
     x = concatenate([x1, x2], axis=3)
-    source_layers.append(x) # 32x32x1024
+    source_layers.append(x) # 32x32x512
 
     x = bl_layer2(x, 256, 1, activation=activation)
     source_layers.append(x) # 16x16x512
@@ -177,6 +158,10 @@ def ssd384x512_dense_body(x, activation='relu'):
     if activation == 'leaky_relu':
         activation = leaky_relu
     
+    growth_rate = 32
+    compression = 1.0
+    source_layers = []
+    
     # Stem
     x = Conv2D(64, 3, strides=2, padding='same')(x)
     x = BatchNormalization(scale=True)(x)
@@ -189,19 +174,11 @@ def ssd384x512_dense_body(x, activation='relu'):
     x = Activation(activation)(x)
     x = MaxPooling2D(pool_size=2, strides=2)(x)
     
-    num_channels = 128
-    growth_rate = 32
-    source_layers = []
+    x = dense_block(x, 6, growth_rate, 4, activation)
+    x = bn_acti_conv(x, int(K.int_shape(x)[3]*compression), 1, 1, activation=activation)
     
-    for i in range(6):
-        x = bl_layer1(x, growth_rate, 4)
-        num_channels += growth_rate
-    x = bn_acti_conv(x, num_channels, activation=activation)
-    
-    for i in range(8):
-        x = bl_layer1(x, growth_rate, 4)
-        num_channels += growth_rate
-    x = bn_acti_conv(x, num_channels, activation=activation)
+    x = dense_block(x, 8, growth_rate, 4, activation)
+    x = bn_acti_conv(x, int(K.int_shape(x)[3]*compression), 1, 1, activation=activation)
     source_layers.append(x)
     
     x = bl_layer2(x, 320, 1, activation=activation)
