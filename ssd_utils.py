c30eebedfc846ff8a50ea1a0b29f14b5d3860dff
Markus Völk <markus.voelk@gmx.de>
2018-09-14 09:11:09 +0200

    move Keras model related utilities to separate file

--- ssd_utils.py
+++ ssd_utils.py
@@ -10,6 +10,8 @@ import os
 
 from tqdm import tqdm
 
+from utils.model_utils import load_weights, calc_memory_usage, count_parameters, plot_parameter_statistic, calc_receptive_field
+
 from ssd_viz import to_rec
 
 
@@ -249,12 +251,12 @@ class PriorMap(object):
         self.priors_variances = priors_variances
         self.priors = np.concatenate([priors_min_xy, priors_max_xy, priors_variances], axis=1)
         
-    def plot_locations(self):
+    def plot_locations(self, color='r'):
         xy = self.box_xy
-        plt.plot(xy[:,0], xy[:,1], 'r.', markersize=4)
+        plt.plot(xy[:,0], xy[:,1], '.', color=color, markersize=6)
     
     def plot_boxes(self, location_idxs=[]):
-        colors = 'rgbcmy'*3
+        colors = 'rgbcmy'
         ax = plt.gca()
         n = self.num_boxes_per_location
         for i in location_idxs:
@@ -264,7 +266,7 @@ class PriorMap(object):
                     break
                 x1, y1, x2, y2 = self.priors[idx, :4]
                 ax.add_patch(plt.Rectangle((x1, y1), x2-x1, y2-y1, 
-                        fill=False, edgecolor=colors[j], linewidth=2))
+                        fill=False, edgecolor=colors[j%len(colors)], linewidth=2))
         ax.autoscale_view()
 
 
@@ -419,7 +421,7 @@ class PriorUtil(object):
         
         priors_xy = self.priors_xy_norm
         priors_wh = self.priors_wh_norm
-        
+
         # assign ground truth to priors
         max_idxs = np.argmax(gt_iou, axis=1)
         max_val = gt_iou[np.arange(num_priors), max_idxs]
@@ -596,175 +598,3 @@ class PriorUtil(object):
         # TODO
         pass
 
-
-def load_weights(model, filepath, layer_names=None):
-    """Loads layer weights from a HDF5 save file.
-     
-    # Arguments
-        model: Keras model
-        filepath: Path to HDF5 file
-        layer_names: List of strings, names of the layers for which the 
-            weights should be loaded. List of tuples, if the names in 
-            the file differ from those in model.
-    """
-    filepath = os.path.expanduser(filepath)
-    f = h5py.File(filepath, 'r')
-    if layer_names == None:
-        layer_names = [s.decode() for s in f.attrs['layer_names']]
-    for name in layer_names:
-        if type(name) in [tuple, list]:
-            layer_name = name[1]
-            name = name[0]
-        else:
-            layer_name = name
-        try:
-            layer = model.get_layer(layer_name)
-            #assert layer is not None
-        except:
-            print('layer missing %s' % (layer_name))
-            continue
-        g = f[name]
-        weights = [np.array(g[wn]) for wn in g.attrs['weight_names']]
-        try:
-            #print('load %s' % (layer_name))
-            layer.set_weights(weights)
-        except Exception as e:
-            print('something went wrong %s' % (layer_name))
-            print('    model %s' % ([w.shape.as_list() for w in layer.weights]))
-            print('    file  %s' % ([w.shape for w in weights]))
-            print(e)
-    f.close()
-
-
-def calc_memory_usage(model, batch_size=1):
-    """Compute the memory usage of a keras modell.
-    
-    # Arguments
-        model: Keras model.
-        batch_size: Batch size used for training.
-    
-    source: https://stackoverflow.com/a/46216013/445710
-    """
-
-    shapes_mem_count = 0
-    for l in model.layers:
-        shapes_mem_count += np.sum([np.sum([np.prod(s[1:]) for s in n.output_shapes]) for n in l._inbound_nodes])
-        
-    trainable_count = np.sum([K.count_params(p) for p in set(model.trainable_weights)])
-    non_trainable_count = np.sum([K.count_params(p) for p in set(model.non_trainable_weights)])
-    
-    # each shape unit occupies 4 bytes in memory
-    total_memory = 4.0 * batch_size * (shapes_mem_count + trainable_count + non_trainable_count)
-    
-    for s in ['Byte', 'KB', 'MB', 'GB', 'TB']:
-        if total_memory > 1024:
-            total_memory /= 1024
-        else:
-            break
-    print('model memory usage %8.2f %s' % (total_memory, s))
-    
-def count_parameters(model):
-    trainable_count = int(np.sum([K.count_params(p) for p in set(model.trainable_weights)]))
-    non_trainable_count = int(np.sum([K.count_params(p) for p in set(model.non_trainable_weights)]))
-    
-    print('trainable     %16i' %(trainable_count))
-    print('non-trainable %16i' %(non_trainable_count))
-    
-def plot_parameter_statistic(model, layer_types=['Dense', 'Conv2D'], trainable=True, non_trainable=False, outputs=False):
-    parameter_count = []
-    names = []
-    for l in model.layers:
-        if l.__class__.__name__ not in layer_types:
-            continue
-        count = 0
-        if outputs:
-            count += np.sum([np.sum([np.prod(s[1:]) for s in n.output_shapes]) for n in l._inbound_nodes])
-        if trainable:
-            count += np.sum([K.count_params(p) for p in set(l.trainable_weights)])
-        if non_trainable:
-            count += np.sum([K.count_params(p) for p in set(l.non_trainable_weights)])
-        parameter_count.append(count)
-        names.append(l.name)
-    
-    y = range(len(names))
-    plt.figure(figsize=[12,len(y)//4])
-    plt.barh(y, parameter_count, align='center')
-    plt.yticks(y, names)
-    plt.ylim(y[0]-1, y[-1]+1)
-    ax = plt.gca()
-    ax.invert_yaxis()
-    ax.xaxis.tick_top()
-    plt.show()
-
-
-def calc_receptive_field(model, layer_name, verbose=False):
-    """Calculate the receptive field related to a certain layer.
-    
-    # Arguments
-        model: Keras model.
-        layer_name: Name of the layer.
-    
-    # Return
-        rf: Receptive field (w, h).
-        es: Effictive stides in the input image.
-        offset: Center of the receptive field associated with the first unit (x, y).
-    """
-    # TODO...
-    
-    fstr = '%-20s %-16s %-10s %-10s %-10s %-16s %-10s %-16s'
-    if verbose:
-        print(fstr % ('name', 'type', 'kernel', 'stride', 'dilation', 'receptive field', 'offset', 'effective stride'))
-    l = model.get_layer(layer_name)
-    rf = np.ones(2)
-    es = np.ones(2)
-    offset = np.zeros(2)
-    
-    while True:
-        layer_type = l.__class__.__name__
-        k, s, d = (1,1), (1,1), (1,1)
-        p = 'same'
-        if layer_type in ['Conv2D']:
-            k = l.kernel_size
-            d = l.dilation_rate
-            s = l.strides
-            p = l.padding
-        elif layer_type in ['MaxPooling2D', 'AveragePooling2D']:
-            k = l.pool_size
-            s = l.strides
-            p = l.padding
-        elif layer_type in ['ZeroPadding2D']:
-            p = l.padding
-        elif layer_type in ['InputLayer', 'Activation', 'BatchNormalization']:
-            pass
-        else:
-            print('unknown layer type %s %s' % (l.name, layer_type))
-            
-        k = np.array(k)
-        s = np.array(s)
-        d = np.array(d)
-        
-        ek = k + (k-1)*(d-1) # effective kernel size
-        rf = rf * s + (ek-s)
-        es = es * s
-        
-        if p == 'valid':
-            offset += ek/2
-            print(ek/2, offset)
-        if type(p) == tuple:
-            offset -= [p[0][0], p[1][0]]
-            print([p[0][0], p[1][0]], offset)
-        
-        rf = rf.astype(int)
-        es = es.astype(int)
-        #offset = offset.astype(int)
-        if verbose:
-            print(fstr % (l.name, l.__class__.__name__, k, s, d, rf, offset, es))
-        
-        if layer_type == 'InputLayer':
-            break
-        
-        input_name = l.input.name.split('/')[0]
-        input_name = input_name.split(':')[0]
-        l = model.get_layer(input_name)
-    
-    return rf, es, offset
---
+++ utils/model_utils.py
@@ -0,0 +1,183 @@
+"""Some utils related to Keras models."""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import keras.backend as K
+import h5py
+import os
+
+
+def load_weights(model, filepath, layer_names=None):
+    """Loads layer weights from a HDF5 save file.
+     
+    # Arguments
+        model: Keras model
+        filepath: Path to HDF5 file
+        layer_names: List of strings, names of the layers for which the 
+            weights should be loaded. List of tuples, if the names in 
+            the file differ from those in model.
+    """
+    filepath = os.path.expanduser(filepath)
+    f = h5py.File(filepath, 'r')
+    if layer_names == None:
+        layer_names = [s.decode() for s in f.attrs['layer_names']]
+    for name in layer_names:
+        if type(name) in [tuple, list]:
+            layer_name = name[1]
+            name = name[0]
+        else:
+            layer_name = name
+        try:
+            layer = model.get_layer(layer_name)
+            #assert layer is not None
+        except:
+            print('layer missing %s' % (layer_name))
+            continue
+        g = f[name]
+        weights = [np.array(g[wn]) for wn in g.attrs['weight_names']]
+        try:
+            #print('load %s' % (layer_name))
+            layer.set_weights(weights)
+        except Exception as e:
+            print('something went wrong %s' % (layer_name))
+            print('    model %s' % ([w.shape.as_list() for w in layer.weights]))
+            print('    file  %s' % ([w.shape for w in weights]))
+            print(e)
+    f.close()
+
+
+def calc_memory_usage(model, batch_size=1):
+    """Compute the memory usage of a keras modell.
+    
+    # Arguments
+        model: Keras model.
+        batch_size: Batch size used for training.
+    
+    source: https://stackoverflow.com/a/46216013/445710
+    """
+
+    shapes_mem_count = 0
+    for l in model.layers:
+        shapes_mem_count += np.sum([np.sum([np.prod(s[1:]) for s in n.output_shapes]) for n in l._inbound_nodes])
+        
+    trainable_count = np.sum([K.count_params(p) for p in set(model.trainable_weights)])
+    non_trainable_count = np.sum([K.count_params(p) for p in set(model.non_trainable_weights)])
+    
+    # each shape unit occupies 4 bytes in memory
+    total_memory = 4.0 * batch_size * (shapes_mem_count + trainable_count + non_trainable_count)
+    
+    for s in ['Byte', 'KB', 'MB', 'GB', 'TB']:
+        if total_memory > 1024:
+            total_memory /= 1024
+        else:
+            break
+    print('model memory usage %8.2f %s' % (total_memory, s))
+
+
+def count_parameters(model):
+    trainable_count = int(np.sum([K.count_params(p) for p in set(model.trainable_weights)]))
+    non_trainable_count = int(np.sum([K.count_params(p) for p in set(model.non_trainable_weights)]))
+    
+    print('trainable     {:>16,d}'.format(trainable_count))
+    print('non-trainable {:>16,d}'.format(non_trainable_count))
+
+
+def plot_parameter_statistic(model, layer_types=['Dense', 'Conv2D'], trainable=True, non_trainable=False, outputs=False):
+    parameter_count = []
+    names = []
+    for l in model.layers:
+        if l.__class__.__name__ not in layer_types:
+            continue
+        count = 0
+        if outputs:
+            count += np.sum([np.sum([np.prod(s[1:]) for s in n.output_shapes]) for n in l._inbound_nodes])
+        if trainable:
+            count += np.sum([K.count_params(p) for p in set(l.trainable_weights)])
+        if non_trainable:
+            count += np.sum([K.count_params(p) for p in set(l.non_trainable_weights)])
+        parameter_count.append(count)
+        names.append(l.name)
+    
+    y = range(len(names))
+    plt.figure(figsize=[12,max(len(y)//4,1)])
+    plt.barh(y, parameter_count, align='center')
+    plt.yticks(y, names)
+    plt.ylim(y[0]-1, y[-1]+1)
+    ax = plt.gca()
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+    plt.show()
+
+
+def calc_receptive_field(model, layer_name, verbose=False):
+    """Calculate the receptive field related to a certain layer.
+    
+    # Arguments
+        model: Keras model.
+        layer_name: Name of the layer.
+    
+    # Return
+        rf: Receptive field (w, h).
+        es: Effictive stides in the input image.
+        offset: Center of the receptive field associated with the first unit (x, y).
+    """
+    # TODO...
+    
+    fstr = '%-20s %-16s %-10s %-10s %-10s %-16s %-10s %-16s'
+    if verbose:
+        print(fstr % ('name', 'type', 'kernel', 'stride', 'dilation', 'receptive field', 'offset', 'effective stride'))
+    l = model.get_layer(layer_name)
+    rf = np.ones(2)
+    es = np.ones(2)
+    offset = np.zeros(2)
+    
+    while True:
+        layer_type = l.__class__.__name__
+        k, s, d = (1,1), (1,1), (1,1)
+        p = 'same'
+        if layer_type in ['Conv2D']:
+            k = l.kernel_size
+            d = l.dilation_rate
+            s = l.strides
+            p = l.padding
+        elif layer_type in ['MaxPooling2D', 'AveragePooling2D']:
+            k = l.pool_size
+            s = l.strides
+            p = l.padding
+        elif layer_type in ['ZeroPadding2D']:
+            p = l.padding
+        elif layer_type in ['InputLayer', 'Activation', 'BatchNormalization']:
+            pass
+        else:
+            print('unknown layer type %s %s' % (l.name, layer_type))
+            
+        k = np.array(k)
+        s = np.array(s)
+        d = np.array(d)
+        
+        ek = k + (k-1)*(d-1) # effective kernel size
+        rf = rf * s + (ek-s)
+        es = es * s
+        
+        if p == 'valid':
+            offset += ek/2
+            print(ek/2, offset)
+        if type(p) == tuple:
+            offset -= [p[0][0], p[1][0]]
+            print([p[0][0], p[1][0]], offset)
+        
+        rf = rf.astype(int)
+        es = es.astype(int)
+        #offset = offset.astype(int)
+        if verbose:
+            print(fstr % (l.name, l.__class__.__name__, k, s, d, rf, offset, es))
+        
+        if layer_type == 'InputLayer':
+            break
+        
+        input_name = l.input.name.split('/')[0]
+        input_name = input_name.split(':')[0]
+        l = model.get_layer(input_name)
+    
+    return rf, es, offset
